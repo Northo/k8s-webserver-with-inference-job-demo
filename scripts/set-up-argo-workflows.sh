8b7f8c01a6d99fe2f2b762818fcdeff033b31ddc
Thorvald M. Ballestad <thorvald@simula.no>
2025-07-15 09:59:28 +0200

    🔥 (scripts): Remove old argo setup scripts

--- scripts/set-up-argo-workflows.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ARGO_WORKFLOWS_VERSION="v3.6.7"
-kubectl create namespace argo
-kubectl apply -n argo -f "https://github.com/argoproj/argo-workflows/releases/download/${ARGO_WORKFLOWS_VERSION}/quick-start-minimal.yaml"
