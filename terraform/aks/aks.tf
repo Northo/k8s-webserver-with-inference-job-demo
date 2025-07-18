d8aa35558ccba8bd3e2322f0d4e6c2f440efd813
Thorvald M. Ballestad <thorvald@simula.no>
2025-07-18 15:28:59 +0200

    🧱 Set node count to two

--- terraform/aks/aks.tf
+++ terraform/aks/aks.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 2
     vm_size    = "Standard_D2_v2"
 
     # Without upgrade_settings, we get a drift (where on every apply, the node pool is updated).
